35d18858e2494ac0c4ad33615155b38a3f18f8d7
Christopher Kennedy <cjkb90@users.noreply.github.com>
2016-01-28 21:47:01 -0500

    Finished through section 7.03

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 var express = require('express');
 var app = express();
 var swig = require('swig');
+var tweetBank = require(__dirname+'/tweetBank.js');
+var routes = require(__dirname+"/routes/");
 app.engine('html',swig.renderFile);
-app.set('view engine','html')
-app.set('views',__dirname+'/views')
+app.set('view engine','html');
+app.set('views',__dirname+'/views');
 swig.setDefaults({cache:false});
-
+app.use('/',routes);
 
 app.use(function(req,res,next){	
 	console.log(req.method+" "+req.url+" "+res.statusCode);
 	next();
 });
 
-app.use('/special',function(req,res,next){	
-	console.log("This is the special area");
-	next();
-});
+// app.get('/',function(req, res){
+// 	res.render('index',locals)
+// });
 
-app.get('/',function(req, res){
-	res.render('index',locals)
-});
-
-app.get('/news',function(req, res){
-	res.send("<h2>This is the news</h2>");
-});
+// app.get('/news',function(req, res){
+// 	res.send("<h2>This is the news</h2>");
+// });
 
 app.listen(3000, function(){
 	console.log("server listening");
